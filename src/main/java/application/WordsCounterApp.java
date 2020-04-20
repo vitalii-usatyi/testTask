f60bb5351730b83625bb7b5f8a27be6a6c4d433c
usatiy <vitalii.usatyi@toweya.net>
2020-04-20 18:57:04 +0300

    Read text from file added

--- src/main/java/application/WordsCounterApp.java
+++ src/main/java/application/WordsCounterApp.java
@@ -2,9 +2,14 @@ package main.java.application;
 
 import main.java.application.service.WordsCounter;
 
+import java.io.IOException;
+
+/**
+ * Enter your text in "text.txt" file
+ */
 public class WordsCounterApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new WordsCounter().run();
     }
 }
--- src/main/java/application/service/WordsCounter.java
+++ src/main/java/application/service/WordsCounter.java
@@ -1,19 +1,29 @@
 package main.java.application.service;
 
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class WordsCounter {
 
-    public void run() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter text:");
-        String text = in.nextLine();
+    public void run() throws IOException {
+        String text = getTextFromFile();
         List<String> words = getWords(text);
         LinkedHashMap<String, Long> result = countWords(words);
-
         result.forEach((word, frequency) -> System.out.println(word + " " + frequency));
+    }
+
+    private String getTextFromFile() throws IOException {
+        File file = new File("src/main/resources/text.txt");
+        List<String> lines = Files.readAllLines(Path.of(file.getAbsolutePath()));
+        return String.join("", lines);
     }
 
     private LinkedHashMap<String, Long> countWords(List<String> listOfWords) {
@@ -34,7 +44,7 @@ public class WordsCounter {
     private List<String> getWords(String text) {
         String[] words = text.split("\\s+");
         for (int i = 0; i < words.length; i++) {
-            words[i] = words[i].replaceAll("[.,_!?;:]", "");
+            words[i] = words[i].replaceAll("[.,_!?;:\'\"]", "");
         }
         return List.of(words);
     }
